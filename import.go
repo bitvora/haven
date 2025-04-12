1e03d39ad26ae1a77664cc8605717cb9d2e2daa4
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-04-13 00:42:52 +0100

    fix: fix timeouts on import and wot
    Also adds better logging.

--- import.go
+++ import.go
@@ -12,13 +12,9 @@ import (
 
 const layout = "2006-01-02"
 
-var (
-	ownerImportedNotes  = 0
-	taggedImportedNotes = 0
-)
-
 func importOwnerNotes() {
-	ctx := context.Background()
+	ownerImportedNotes := 0
+	nFailedImportNotes := 0
 	wdb := eventstore.RelayWrapper{Store: outboxDB}
 
 	startTime, err := time.Parse(layout, config.ImportStartDate)
@@ -29,63 +25,104 @@ func importOwnerNotes() {
 	endTime := startTime.Add(240 * time.Hour)
 
 	for {
-		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-		defer cancel()
-
 		startTimestamp := nostr.Timestamp(startTime.Unix())
 		endTimestamp := nostr.Timestamp(endTime.Unix())
 
-		filters := []nostr.Filter{{
+		filter := nostr.Filter{
 			Authors: []string{nPubToPubkey(config.OwnerNpub)},
 			Since:   &startTimestamp,
 			Until:   &endTimestamp,
-		}}
-
-		for ev := range pool.SubManyEose(ctx, config.ImportSeedRelays, filters) {
-			wdb.Publish(ctx, *ev.Event)
-			ownerImportedNotes++
 		}
-		log.Println("📦 imported", ownerImportedNotes, "owner notes")
-		time.Sleep(5 * time.Second)
+
+		done := make(chan int, 1)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+		go func() {
+			defer cancel()
+			batchImportedNotes := 0
+
+			pool.FetchManyReplaceable(ctx, config.ImportSeedRelays, filter).Range(func(_ nostr.ReplaceableKey, ev *nostr.Event) bool {
+				if ctx.Err() != nil {
+					return false // Stop the loop on timeout
+				}
+				if err := wdb.Publish(ctx, *ev); err != nil {
+					log.Println("🚫  error importing note", ev.ID, ":", err)
+					nFailedImportNotes++
+					return true
+				}
+				batchImportedNotes++
+				return true
+			})
+			done <- batchImportedNotes
+			close(done)
+		}()
+
+		select {
+		case batchImportedNotes := <-done:
+			ownerImportedNotes += batchImportedNotes
+			if batchImportedNotes == 0 {
+				log.Printf("ℹ️ No notes found for %s to %s", startTime.Format(layout), endTime.Format(layout))
+			} else {
+				log.Printf("📦 Imported %d notes from %s to %s", batchImportedNotes, startTime.Format(layout), endTime.Format(layout))
+			}
+		case <-ctx.Done():
+			log.Printf("🚫 Timeout after %v while importing notes from %s to %s", 30*time.Second, startTime.Format(layout), endTime.Format(layout))
+		}
 
 		startTime = startTime.Add(240 * time.Hour)
 		endTime = endTime.Add(240 * time.Hour)
 
 		if startTime.After(time.Now()) {
-			log.Println("✅ owner note import complete! ")
+			log.Println("✅ owner note import complete! Imported", ownerImportedNotes, "notes")
 			break
+		}
+		if nFailedImportNotes > 0 {
+			log.Printf("⚠️ Failed to import %d notes", nFailedImportNotes)
 		}
 	}
 }
 
 func importTaggedNotes() {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	taggedImportedNotes := 0
+	done := make(chan struct{}, 1)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 	defer cancel()
 
 	wdb := eventstore.RelayWrapper{Store: inboxDB}
-	filters := []nostr.Filter{{
+	filter := nostr.Filter{
 		Tags: nostr.TagMap{
 			"p": {nPubToPubkey(config.OwnerNpub)},
 		},
-	}}
-
-	log.Println("📦 importing inbox notes, please wait 2 minutes")
-	taggedImportedNotes = 0
-	for ev := range pool.SubMany(ctx, config.ImportSeedRelays, filters) {
-		if !wotMap[ev.Event.PubKey] {
-			continue
-		}
-		for _, tag := range ev.Event.Tags.GetAll([]string{"p"}) {
-			if len(tag) < 2 {
-				continue
-			}
-			if tag[1] == nPubToPubkey(config.OwnerNpub) {
-				wdb.Publish(ctx, *ev.Event)
-				taggedImportedNotes++
-			}
-		}
 	}
+
+	log.Println("📦 importing inbox notes, please wait up to 2 minutes")
+
+	go func() {
+		pool.FetchManyReplaceable(ctx, config.ImportSeedRelays, filter).Range(func(_ nostr.ReplaceableKey, ev *nostr.Event) bool {
+			if ctx.Err() != nil {
+				return false // Stop the loop on timeout
+			}
+			if !wotMap[ev.PubKey] {
+				return true
+			}
+			for tag := range ev.Tags.FindAll("p") {
+				if len(tag) < 2 {
+					continue
+				}
+				if tag[1] == nPubToPubkey(config.OwnerNpub) {
+					if err := wdb.Publish(ctx, *ev); err != nil {
+						log.Println("error importing tagged note", ev.ID, ":", err)
+						return true
+					}
+					taggedImportedNotes++
+				}
+			}
+
+			return true
+		})
+		close(done)
+	}()
+
 	log.Println("📦 imported", taggedImportedNotes, "tagged notes")
 	log.Println("✅ tagged import complete. please restart the relay")
 }
@@ -94,24 +131,29 @@ func subscribeInbox() {
 	ctx := context.Background()
 	wdb := eventstore.RelayWrapper{Store: inboxDB}
 	startTime := nostr.Timestamp(time.Now().Add(-time.Minute * 5).Unix())
-	filters := []nostr.Filter{{
+	filter := nostr.Filter{
 		Tags: nostr.TagMap{
 			"p": {nPubToPubkey(config.OwnerNpub)},
 		},
 		Since: &startTime,
-	}}
+	}
 
 	log.Println("📢 subscribing to inbox")
-	for ev := range pool.SubMany(ctx, config.ImportSeedRelays, filters) {
+
+	for ev := range pool.SubscribeMany(ctx, config.ImportSeedRelays, filter) {
 		if !wotMap[ev.Event.PubKey] {
 			continue
 		}
-		for _, tag := range ev.Event.Tags.GetAll([]string{"p"}) {
+		for tag := range ev.Event.Tags.FindAll("p") {
 			if len(tag) < 2 {
 				continue
 			}
 			if tag[1] == nPubToPubkey(config.OwnerNpub) {
-				wdb.Publish(ctx, *ev.Event)
+				if err := wdb.Publish(ctx, *ev.Event); err != nil {
+					log.Println("error importing tagged note", ev.Event.ID, ":", "from relay", ev.Relay.URL, ":", err)
+					continue
+				}
+
 				switch ev.Event.Kind {
 				case nostr.KindTextNote:
 					log.Println("📰 new note in your inbox")
@@ -126,9 +168,8 @@ func subscribeInbox() {
 				case nostr.KindFollowList:
 					// do nothing
 				default:
-					log.Println("📦 new event in your inbox")
+					log.Println("📦 new event kind", ev.Event.Kind, "event in your inbox")
 				}
-				taggedImportedNotes++
 			}
 		}
 	}
