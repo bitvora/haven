ef4bc0c0186ea55118cbf81fcd61a807b7c96b49
Sergey B. <sergey3bv@gmail.com>
2026-01-16 16:53:04 +0300

    feat: updating wot refresh interval from hours to duration

--- config.go
+++ config.go
@@ -7,9 +7,12 @@ import (
 	"runtime/debug"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 )
+
+const DEFAULT_WOT_REFRESH_INTERVAL time.Duration = 24 * time.Hour
 
 type AwsConfig struct {
 	AccessKeyID     string `json:"access"`
@@ -31,52 +34,58 @@ type S3Config struct {
 }
 
 type Config struct {
-	OwnerNpub                            string     `json:"owner_npub"`
-	DBEngine                             string     `json:"db_engine"`
-	LmdbMapSize                          int64      `json:"lmdb_map_size"`
-	BlossomPath                          string     `json:"blossom_path"`
-	RelayURL                             string     `json:"relay_url"`
-	RelayPort                            int        `json:"relay_port"`
-	RelayBindAddress                     string     `json:"relay_bind_address"`
-	RelaySoftware                        string     `json:"relay_software"`
-	RelayVersion                         string     `json:"relay_version"`
-	PrivateRelayName                     string     `json:"private_relay_name"`
-	PrivateRelayNpub                     string     `json:"private_relay_npub"`
-	PrivateRelayDescription              string     `json:"private_relay_description"`
-	PrivateRelayIcon                     string     `json:"private_relay_icon"`
-	ChatRelayName                        string     `json:"chat_relay_name"`
-	ChatRelayNpub                        string     `json:"chat_relay_npub"`
-	ChatRelayDescription                 string     `json:"chat_relay_description"`
-	ChatRelayIcon                        string     `json:"chat_relay_icon"`
-	ChatRelayWotDepth                    int        `json:"chat_relay_wot_depth"`
-	ChatRelayWotRefreshIntervalHours     int        `json:"chat_relay_wot_refresh_interval_hours"`
-	ChatRelayMinimumFollowers            int        `json:"chat_relay_minimum_followers"`
-	OutboxRelayName                      string     `json:"outbox_relay_name"`
-	OutboxRelayNpub                      string     `json:"outbox_relay_npub"`
-	OutboxRelayDescription               string     `json:"outbox_relay_description"`
-	OutboxRelayIcon                      string     `json:"outbox_relay_icon"`
-	InboxRelayName                       string     `json:"inbox_relay_name"`
-	InboxRelayNpub                       string     `json:"inbox_relay_npub"`
-	InboxRelayDescription                string     `json:"inbox_relay_description"`
-	InboxRelayIcon                       string     `json:"inbox_relay_icon"`
-	InboxPullIntervalSeconds             int        `json:"inbox_pull_interval_seconds"`
-	ImportStartDate                      string     `json:"import_start_date"`
-	ImportOwnerNotesFetchTimeoutSeconds  int        `json:"import_owned_notes_fetch_timeout_seconds"`
-	ImportTaggedNotesFetchTimeoutSeconds int        `json:"import_tagged_fetch_timeout_seconds"`
-	ImportQueryIntervalSeconds           int        `json:"import_query_interval_seconds"`
-	ImportSeedRelays                     []string   `json:"import_seed_relays"`
-	BackupProvider                       string     `json:"backup_provider"`
-	BackupIntervalHours                  int        `json:"backup_interval_hours"`
-	WotFetchTimeoutSeconds               int        `json:"wot_fetch_timeout_seconds"`
-	LogLevel                             string     `json:"log_level"`
-	BlastrRelays                         []string   `json:"blastr_relays"`
-	AwsConfig                            *AwsConfig `json:"aws_config"`
-	S3Config                             *S3Config  `json:"s3_config"`
-	GcpConfig                            *GcpConfig `json:"gcp_config"`
+	OwnerNpub                            string        `json:"owner_npub"`
+	DBEngine                             string        `json:"db_engine"`
+	LmdbMapSize                          int64         `json:"lmdb_map_size"`
+	BlossomPath                          string        `json:"blossom_path"`
+	RelayURL                             string        `json:"relay_url"`
+	RelayPort                            int           `json:"relay_port"`
+	RelayBindAddress                     string        `json:"relay_bind_address"`
+	RelaySoftware                        string        `json:"relay_software"`
+	RelayVersion                         string        `json:"relay_version"`
+	PrivateRelayName                     string        `json:"private_relay_name"`
+	PrivateRelayNpub                     string        `json:"private_relay_npub"`
+	PrivateRelayDescription              string        `json:"private_relay_description"`
+	PrivateRelayIcon                     string        `json:"private_relay_icon"`
+	ChatRelayName                        string        `json:"chat_relay_name"`
+	ChatRelayNpub                        string        `json:"chat_relay_npub"`
+	ChatRelayDescription                 string        `json:"chat_relay_description"`
+	ChatRelayIcon                        string        `json:"chat_relay_icon"`
+	ChatRelayWotDepth                    int           `json:"chat_relay_wot_depth"`
+	ChatRelayWotRefreshInterval          time.Duration `json:"chat_relay_wot_refresh_interval"`
+	ChatRelayMinimumFollowers            int           `json:"chat_relay_minimum_followers"`
+	OutboxRelayName                      string        `json:"outbox_relay_name"`
+	OutboxRelayNpub                      string        `json:"outbox_relay_npub"`
+	OutboxRelayDescription               string        `json:"outbox_relay_description"`
+	OutboxRelayIcon                      string        `json:"outbox_relay_icon"`
+	InboxRelayName                       string        `json:"inbox_relay_name"`
+	InboxRelayNpub                       string        `json:"inbox_relay_npub"`
+	InboxRelayDescription                string        `json:"inbox_relay_description"`
+	InboxRelayIcon                       string        `json:"inbox_relay_icon"`
+	InboxPullIntervalSeconds             int           `json:"inbox_pull_interval_seconds"`
+	ImportStartDate                      string        `json:"import_start_date"`
+	ImportOwnerNotesFetchTimeoutSeconds  int           `json:"import_owned_notes_fetch_timeout_seconds"`
+	ImportTaggedNotesFetchTimeoutSeconds int           `json:"import_tagged_fetch_timeout_seconds"`
+	ImportQueryIntervalSeconds           int           `json:"import_query_interval_seconds"`
+	ImportSeedRelays                     []string      `json:"import_seed_relays"`
+	BackupProvider                       string        `json:"backup_provider"`
+	BackupIntervalHours                  int           `json:"backup_interval_hours"`
+	WotFetchTimeoutSeconds               int           `json:"wot_fetch_timeout_seconds"`
+	LogLevel                             string        `json:"log_level"`
+	BlastrRelays                         []string      `json:"blastr_relays"`
+	AwsConfig                            *AwsConfig    `json:"aws_config"`
+	S3Config                             *S3Config     `json:"s3_config"`
+	GcpConfig                            *GcpConfig    `json:"gcp_config"`
 }
 
 func loadConfig() Config {
 	_ = godotenv.Load(".env")
+
+	wotRefreshIntervalEnv := getEnv("CHAT_RELAY_WOT_REFRESH_INTERVAL")
+	wotRefreshInterval, err := time.ParseDuration(wotRefreshIntervalEnv)
+	if err != nil {
+		wotRefreshInterval = DEFAULT_WOT_REFRESH_INTERVAL
+	}
 
 	return Config{
 		OwnerNpub:                            getEnv("OWNER_NPUB"),
@@ -97,7 +106,7 @@ func loadConfig() Config {
 		ChatRelayDescription:                 getEnv("CHAT_RELAY_DESCRIPTION"),
 		ChatRelayIcon:                        getEnv("CHAT_RELAY_ICON"),
 		ChatRelayWotDepth:                    getEnvInt("CHAT_RELAY_WOT_DEPTH", 0),
-		ChatRelayWotRefreshIntervalHours:     getEnvInt("CHAT_RELAY_WOT_REFRESH_INTERVAL_HOURS", 0),
+		ChatRelayWotRefreshInterval:          wotRefreshInterval,
 		ChatRelayMinimumFollowers:            getEnvInt("CHAT_RELAY_MINIMUM_FOLLOWERS", 0),
 		OutboxRelayName:                      getEnv("OUTBOX_RELAY_NAME"),
 		OutboxRelayNpub:                      getEnv("OUTBOX_RELAY_NPUB"),
